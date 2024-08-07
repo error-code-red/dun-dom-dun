926c0cf5247c19b3431386952517f4c145d3cc0d
error-code-red <149626268+error-code-red@users.noreply.github.com>
2024-08-07 01:30:14 +0000

    gg

---
+++ dom.js
@@ -0,0 +1,23 @@
+function test(){
+ alert("Greetings!")
+}
+
+test();
+
+let dark = false;
+
+let myButton = document.querySelector("Button")
+
+function handleClick(){
+    alert("Button :o")
+}
+
+myButton.addEventListener('click',handleClick)
+
+function darkMode(){
+ document.body.style.backgroundColor = "rgb(0,0,0)"
+ document.querySelector("#texty").
+ dark = true;
+}
+
+myButton.addEventListener('click', darkMode)
